6529b054268abbb2d2346556fde098294e88b8c5
davs1234 <velascos.davida@gmail.com>
2013-02-20 19:24:30 -0600

    Update src/sh.c
    Ahora tiene la funcion addVar, ando trabajando que pueda ejecutar programas con el commando export. Despues voy a ver como agregarles valores a las variables.

--- src/sh.c
+++ src/sh.c
@@ -10,8 +10,8 @@ char * buffer;
 
 int main(int argc, char **argv) {
 	HashTable *table = createHashTable(100);
+	sleep(30);
 	//int exitCalled = 0;
-
 	char *command;
 	while(1) {
 		askUser("sh> ", 100);
@@ -29,40 +29,59 @@ int main(int argc, char **argv) {
 				if(nextWord != NULL && nextWord[0] == '$') {
 					memmove(nextWord, nextWord + 1, strlen(nextWord));
 					Var *res = findVar(table, nextWord);
-					if(res == NULL)
-						printf("	ERROR: Variable %s not declared.\n", nextWord);
-					else
-						printf("	%s\n",res->val);
+						if(res == NULL)
+							printf(" ERROR: Variable %s not declared.\n", nextWord);
+						else
+							printf(" %s\n",res->val);
 
 				} else {
-					printf("	ERROR: Expected variable query symbol ($).\n");
+					printf(" ERROR: Expected variable query symbol ($).\n");
 				}
-			} else if(strcmp(nextWord, "export") == 0) { 
+			} else if(strcmp(nextWord, "export") == 0) {
 				nextWord = strtok(NULL,"=");
-				if(nextWord != NULL) {
-					char *var = strdup(nextWord);
-					nextWord = strtok(NULL, " ");
 					if(nextWord != NULL) {
-						setVar(table, var, nextWord);
-						//printf("	KEY %s, VAL: %s", var, nextWord);
+						char *var = strdup(nextWord);
+							if(nextWord != NULL) {
+								//setVar(table, var, nextWord);
+								//printf(" KEY %s, VAL: %s", var, nextWord);
+								int pid;
+								char *argv[] = {"xterm", "-e", nextWord, (char *) 0 };
+								pid = execvp("xterm", argv);
+							} else {
+								printf(" ERROR: Missing value.\n");
+							}
 					} else {
-						printf("	ERROR: Missing value.\n");
-					}
-					
-				} else {
-					printf("	ERROR: Expected variable - value pair.\n");
+						printf(" ERROR: Expected variable - value pair.\n");
+						}
 				}
-			}
+				else if(strcmp(nextWord, "addVar") == 0) {
+					nextWord = strtok(NULL, " ");
+					if(nextWord != NULL)
+					{
+						char *var = strdup(nextWord);
+						if(nextWord != NULL) {
+							setVar(table, var, nextWord);
+							printf("Variable %s has been added\n", nextWord);
+						}
+						else {
+							printf("ERROR: Missing variable name\n");
+						}
+						getchar();
+					}else
+					{
+						printf("ERROR: Expecte variable - value pair.\n");
+					}
+				}
+				else{
+					printf("Not a recognize command\n");
+				}
 
-		    wordCount++;
+				wordCount++;
 
-		    nextWord = strtok(NULL," ");
+				nextWord = strtok(NULL," ");
 
 		}
 	}
-
-
-
 	freeTable(table);
 	return 0;
 }
@@ -71,14 +90,15 @@ int main(int argc, char **argv) {
 char *askUser(char *question, int length) {
 	char text[length];
 	buffer = (char*) malloc(sizeof(char) * length);
-   	fputs(question, stdout);
-   	fflush(stdout);
-   	if ( fgets(text, sizeof text, stdin) != NULL ) {
-    	char *newline = strchr(text, '\n'); // Search new line
-      	if ( newline != NULL ) {
-        	*newline = '\0';
-      	}
-      	strcpy(buffer, text);
-   	}
-   	return "";
+	//printf("%s ", question);
+    fputs(question, stdout);
+    fflush(stdout);
+    if ( fgets(text, sizeof text, stdin) != NULL ) {
+     char *newline = strchr(text, '\n'); // Search new line
+       if ( newline != NULL ) {
+         *newline = '\0';
+       }
+       strcpy(buffer, text);
+    }
+    return "";
 }
