fe936a0698d44a3e5c565b1c05cf98be4a00d55d
davs1234 <velascos.davida@gmail.com>
2013-02-20 19:21:25 -0600

    Update src/init.c
    no me acuerdo si le hice cambio aqui o no,le quite commentarios.

--- src/init.c
+++ src/init.c
@@ -3,60 +3,52 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-/*
- * Perdon por el abuso de comentarios aqui, pero quiero que este tenga todo super explicado
- * despues lo borran, o lo dejan como apoyo para cuando sea la revicion en clase.
- * Esta clase, segun yo ya esta completa
- * Ya genera los 6 hijos de golpe que es lo unico que realmente debe hacer la clase init
- * */
-
 int newTerm();
 
 int main()
 {
-  //Declaración de variables
-	int i = 0;															//Contador para la generacion de los 6 procesos
-	int pid = -1;														//La variable que tendra el ID de los procesos que se van creando
+	//Declaración de variables
+	int i = 0;	//Contador para la generacion de los 6 procesos
+	int pid = -1;	//La variable que tendra el ID de los procesos que se van creando
 	int pids[6];
 	int status = -1;
 	int killall = 0;
 	/*
-	 * Ciclo encargado de crear los sub-procesos
-	 * */
+	* Ciclo encargado de crear los sub-procesos
+	* */
 	for(i = 0; i < 6; i++) {
 		pids[i] = newTerm();
 		printf("Process PID%d started\n", pids[i]);
 	}
-
-	while (i > 0) {
-  		pid = wait(&status);
-  		printf("PID %d exited with status %x.  %d \n", pid, status, WEXITSTATUS(status));
-
-  		status = readStatus("status");
-
-  		if(status == 0 && !killall) {
-  			
-  			int n;
-  			for(n = 0; n < 6; n++) {
-  				if(pids[n] == pid) break;
-  			}
-  			printf("Restarting closed process #%d\n", n);
-  			while( (pid = newTerm()) == -1);
-			pids[n] = pid;  			
-
-  			++i;
-  		} else if(status == 123) {
-  			printf("Received shutdown signal from PID: %d\n", pid);
-  			//kill(pid);
-  			killall = 1;
-  			for(i = 0; i < 6; i++) {
-  				kill(pids[i], SIGUSR1);
-  			}
-  		}
-
-  		--i;  // TODO(pts): Remove pid from the pids array.
-	}
 	
+	while (i > 0) {
+		pid = wait(&status);
+		printf("PID %d exited with status %x. %d \n", pid, status, WEXITSTATUS(status));
+
+		status = readStatus("status");
+		
+		if(status == 0 && !killall) {
+  
+			int n;
+			for(n = 0; n < 6; n++) {
+				if(pids[n] == pid) break;
+			}
+			printf("Restarting closed process #%d\n", n);
+			while( (pid = newTerm()) == -1);
+			pids[n] = pid;
+			
+			++i;
+		} else if(status == 123) {
+			printf("Received shutdown signal from PID: %d\n", pid);
+			//kill(pid);
+			killall = 1;
+			for(i = 0; i < 6; i++) {
+				kill(pids[i], SIGUSR1);
+			}
+		}
+		--i; // TODO(pts): Remove pid from the pids array.
+	}
+
 	return 0;
 }
 
